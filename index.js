e7d34b3fb23cbd1834807a972d63263e74171db6
TA2LSM <semihsenol@gmail.com>
2022-03-11 15:23:36 +0300

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose
   });
 
 const courseSchema = new mongoose.Schema({
-  //_id: mongoose.ObjectId,
+  _id: mongoose.ObjectId,
   //bu kısım önceki kodlarda yok. .findById() çalışmadığı için araştırıldı id'ler database'de
   //ObjectId olarak tanımlı olması gerekirken string olduğu için çalışmıyormuş.
   name: String,
@@ -129,9 +129,9 @@ async function updateCourse(id) {
   // Approach 2: Update first, update directly, optionally get the updated document
   // MongoDB update operator kısmına kendi web sitesinden bakılabilir.
   // $currentDate, $inc, $min, $max, $mul, $rename, $set, $unSet, $setOnInsert ...vs
-  // Fakat burada kurstaki yöntem kullanılamadı için bu operatörlere gerek kalmadı !!!
+  // Fakat burada kurstaki yöntem kullanılamadığı için bu operatörlere gerek kalmadı !!!
 
-  // Aşağıdaki iki yöntem de çalışıyor...
+  // Aşağıda farklı farklı yöntemler gösterilmiştir...
   // (1)
   // const result = await Course.findByIdAndUpdate(id, {
   //   isPublished: true,
@@ -139,17 +139,78 @@ async function updateCourse(id) {
   // });
   // console.log(result);
 
-  // (2)
-  const result = await Course.findOneAndUpdate(id, {
-    isPublished: true,
-    author: "Modified Author!",
-  });
-  console.log(result);
+  // const course = await Course.findOne({ _id: id });
+  // console.log(course);
 
-  const course = await Course.findOne({ _id: id });
+  // (1_1)
+  // const course = await Course.findByIdAndUpdate(
+  //   id,
+  //   {
+  //     isPublished: true,
+  //     author: "Modified Author!",
+  //   },
+  //   { new: true } // bunu yazarsak değiştirilmiş dökümanı döner. Tekrar aramamıza gerek kalmaz.
+  // );
+  // console.log(course);
+
+  // (2)
+  const course = await Course.findOneAndUpdate(
+    id,
+    {
+      isPublished: true,
+      author: "Modified Author!",
+    },
+    { new: true }
+  );
   console.log(course);
+
+  // const course = await Course.findOne({ _id: id });
+  // console.log(course);
 }
 
 // MongoDB Compass'tan geçerli _id'ler alındı.
 //updateCourse("5a68fdf95db93f6477053ddd"); // not published course
-updateCourse("5a68fdd7bee8ea64649c2777"); // published course
+//updateCourse("5a68fdd7bee8ea64649c2777"); // published course
+
+//--- 19. Video ---
+async function removeCourse(id) {
+  //const result = Course.deleteOne({ isPublished: true });
+  // kritere uyan birden fazla sonuç varsa sadece ilkini siler.
+  //(1)
+  // const result = await Course.deleteOne({ _id: id });
+  // console.log(result);
+  //(2)
+  // const result = await Course.deleteMany({ isPublished: false });
+  // console.log(result);
+  //(3)
+  // .findByIdAndDelete() metodu da var
+  const course = await Course.findByIdAndRemove(id);
+  // ilgili kurs silinmişse ya da yoksa
+  if (!course) {
+    console.log("The course was not found!");
+    return;
+  }
+  console.log(course);
+}
+
+//removeCourse("5a68fdd7bee8ea64649c2777");
+removeCourse("622b3e8e59bde2a4d15a8189");
+
+// Ek olarak silinmiş kursu yerine koymak için yazıldı...
+async function createCourse() {
+  const course = new Course({
+    _id: "622b3e8e59bde2a4d15a8189",
+    tags: ["node", "backend"],
+    date: "2018-01-24T21:42:47.912Z",
+    name: "Node.js Course (2)",
+    author: "TA2LSM",
+    isPublished: true,
+    price: 18,
+    __v: 0,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+//createCourse();
